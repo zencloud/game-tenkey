8d865406e7ebe5cba17bdab21c27447a2866e4e6
zencloud <mryanmill@gmail.com>
2019-05-26 16:01:57 -0600

    Dev Backup

--- assets/js/game-functions.js
+++ assets/js/game-functions.js
@@ -9,10 +9,40 @@ gameData = {
 
     // Mole Loop Controls
     moleLoopTimeout: -1,
+
+    // Game Difficulty/Speed
+    gameSpeedOptions: {
+        "normal": [350, 1000],
+        "fast": [250, 750],
+        "crazy": [150, 450],
+    },
+
+    gameSpeedCurrent: [],
+    gameLoopRunning: false,
+
+    // Player Stats
+    playerInputCorrectTotal: 0,
+    playerInputWrongTotal: 0
 }
 
-function fx_create_flash (cellNumber) {
-    
+// Update Gamespeed
+gameData.gameSpeedCurrent = gameData.gameSpeedOptions.normal;
+
+// --- Update DOM UI Game Info
+function game_ui_update_game_info() {
+
+    // Get DOM elements
+    let correctDiv = document.getElementById("game-info-input-correct-total");
+    let wrongDiv = document.getElementById("game-info-input-wrong-total");
+
+    // Update Elements with current info
+    correctDiv.textContent = gameData.playerInputCorrectTotal;
+    wrongDiv.textContent = gameData.playerInputWrongTotal;
+}
+
+// --- FX: Create Impact Flash
+function fx_create_flash(cellNumber) {
+
     let cellID = "c" + cellNumber;
     let moleDiv = document.getElementById(cellID);
     let flashDiv = document.createElement("div");
@@ -27,21 +57,21 @@ function fx_create_flash (cellNumber) {
 
 // --- Mole is created at cell spot
 function game_mole_create() {
-    
+
     // Get array of all free cells
     let freeCells = [];
     for (let i = 0; i < gameData.cellMatrix.length; i++) {
         if (gameData.cellMatrix[i] == 0) {
-            freeCells.push(i+1);
+            freeCells.push(i + 1);
         }
     }
 
     // Select 1 cell from free cellsq
-    let foundCell = freeCells[getRandomInt(0, freeCells.length-1)];
+    let foundCell = freeCells[getRandomInt(0, freeCells.length - 1)];
 
     // Update Cell Matrix
-    gameData.cellMatrix[foundCell-1] = 1;
-    
+    gameData.cellMatrix[foundCell - 1] = 1;
+
     // Find cell in DOM and generate mole
     let cellID = "c" + foundCell;
     let moleDiv = document.getElementById(cellID).getElementsByClassName("mole-img")[0];
@@ -49,10 +79,10 @@ function game_mole_create() {
         <img data-cell="${foundCell}" class="animate-pop-up" src="assets/imgs/mole.png">`;
 
     // Reset Mole Delay
-    gameData.timeoutMatrix[foundCell-1] = setTimeout(() => {
+    gameData.timeoutMatrix[foundCell - 1] = setTimeout(() => {
 
         // Update Matrix
-        gameData.cellMatrix[foundCell-1] = 0;
+        gameData.cellMatrix[foundCell - 1] = 0;
 
         // Update with image change
         moleDiv.innerHTML = `
@@ -62,51 +92,70 @@ function game_mole_create() {
 
 
 // --- Get Player Input
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", function (event) {
 
     // Check if Number
+
     if (!isNaN(event.key)) {
-        
-        // Check Matrix against input
+
         // Convert key to number
         let cellNumber = parseInt(event.key);
-        if (gameData.cellMatrix[cellNumber-1] === 1) {
 
+        // Wrong Input
+        if (gameData.cellMatrix[cellNumber - 1] === 0) {
+
+            // Update Game Value: Correct Inputs
+            gameData.playerInputWrongTotal++;
+
+            // Update Game Info Dom
+            game_ui_update_game_info();
+        }
+
+        // Correct Input
+        if (gameData.cellMatrix[cellNumber - 1] === 1) {
+
+            // Update Game Value: Correct Inputs
+            gameData.playerInputCorrectTotal++;
+
+            // Update Game Info Dom
+            game_ui_update_game_info();
 
             // Update Matrix
-            gameData.cellMatrix[cellNumber-1] = 0;
+            gameData.cellMatrix[cellNumber - 1] = 0;
 
             // Update Mole hit
             let cellID = "c" + cellNumber;
             let moleDiv = document.getElementById(cellID).getElementsByClassName("mole-img")[0];
-            
-            // Creat FX
-            fx_create_flash(cellNumber);
-
-            // Disable Mole Timeout
-            clearTimeout(gameData.timeoutMatrix[cellNumber-1]);
 
             // Update with image change
             moleDiv.innerHTML = `
             <img class="animate-drop-down" src="assets/imgs/mole_damages.png">`;
-            
+
+            // Creat FX
+            fx_create_flash(cellNumber);
+
+            // Disable Mole Timeout
+            clearTimeout(gameData.timeoutMatrix[cellNumber - 1]);
         }
     }
 });
 
 
 // --- Mole Loop Start Running
-function game_mole_loop_start () {
-
-    let timeDelay = getRandomInt(300, 1000);
+function game_mole_loop_start() {
+    gameData.gameLoopRunning = true;
+    let timeDelay = getRandomInt(gameData.gameSpeedCurrent[0], gameData.gameSpeedCurrent[1]);
     gameData.moleLoopTimeout = setTimeout(() => {
         game_mole_create();
         game_mole_loop_start();
     }, timeDelay);
 }
 
+
 // --- Mole Loop Stop Running
-function game_mole_loop_stop () {
+function game_mole_loop_stop() {
     clearTimeout(gameData.moleLoopTimeout);
     gameData.moleLoopTimeout = -1;
+    gameData.gameLoopRunning = false;
+
 }
